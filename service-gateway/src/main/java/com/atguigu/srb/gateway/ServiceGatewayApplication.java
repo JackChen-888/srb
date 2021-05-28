03548b4642a12731afd4d55327e27979e889a93e
liuchen <liuchen525@126.com>
2021-05-28 16:21:39 +0800

    项目服务网关

---
+++ service-gateway/src/main/java/com/atguigu/srb/gateway/ServiceGatewayApplication.java
@@ -0,0 +1,18 @@
+package com.atguigu.srb.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author: chen
+ * @date: 2021年05月28日 16:18
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ServiceGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceGatewayApplication.class, args);
+    }
+}
