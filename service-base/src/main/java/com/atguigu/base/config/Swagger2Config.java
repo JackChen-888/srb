56af4b3cb4d92d2acd22f18d61cb34f9a93b9742
liuchen <liuchen525@126.com>
2021-05-28 14:13:10 +0800

    swagger可查询全部

--- service-base/src/main/java/com/atguigu/base/config/Swagger2Config.java
+++ service-base/src/main/java/com/atguigu/base/config/Swagger2Config.java
@@ -26,7 +26,7 @@ public class Swagger2Config {
                 .apiInfo(adminApiInfo())
                 .select()
                 //只显示admin路径下的页面
-                .paths(Predicates.and(PathSelectors.regex("/admin/.*")))
+//                .paths(Predicates.and(PathSelectors.regex("/admin/.*")))
                 .build();
 
     }
