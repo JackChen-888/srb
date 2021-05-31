e714c1a4dab0c9c728569287358012a43ec6ec84
liuchen <liuchen525@126.com>
2021-05-31 11:03:38 +0800

    错误修改

--- service-core/src/main/java/com/atguigu/srb/core/controller/UserBindController.java
+++ service-core/src/main/java/com/atguigu/srb/core/controller/UserBindController.java
@@ -1,9 +1,20 @@
 package com.atguigu.srb.core.controller;
 
 
+import com.atguigu.base.util.JwtUtils;
+import com.atguigu.common.result.R;
+import com.atguigu.srb.core.pojo.vo.UserBindVO;
+import com.atguigu.srb.core.service.UserBindService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * <p>
@@ -13,9 +24,23 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Chen
  * @since 2021-05-21
  */
+@Api(tags = "会员账号绑定")
 @RestController
-@RequestMapping("/userBind")
+@RequestMapping("/api/core/userBind")
+@Slf4j
 public class UserBindController {
 
+    @Resource
+    private UserBindService userBindService;
+
+    @ApiOperation("账户绑定提交数据")
+    @PostMapping("/auth/bind")
+    public R bind(@RequestBody UserBindVO userBindVO, HttpServletRequest request) {
+        String token = request.getHeader("token");
+        Long userId = JwtUtils.getUserId(token);
+        String formStr = userBindService.commitBindUser(userBindVO, userId);
+        return R.ok().data("formStr", formStr);
+    }
 }
+
 
