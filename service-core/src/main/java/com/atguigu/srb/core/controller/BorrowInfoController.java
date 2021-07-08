3c4bf0c6392476fe442c98340729ca7fabe1cc9b
liuchen <liuchen525@126.com>
2021-07-08 16:33:36 +0800

    借款申请

--- service-core/src/main/java/com/atguigu/srb/core/controller/BorrowInfoController.java
+++ service-core/src/main/java/com/atguigu/srb/core/controller/BorrowInfoController.java
@@ -1,9 +1,17 @@
 package com.atguigu.srb.core.controller;
 
+import com.atguigu.base.util.JwtUtils;
+import com.atguigu.common.result.R;
+import com.atguigu.srb.core.pojo.entity.BorrowInfo;
+import com.atguigu.srb.core.service.BorrowInfoService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.math.BigDecimal;
 
 /**
  * <p>
@@ -13,9 +21,39 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Chen
  * @since 2021-05-21
  */
+@Api(tags = "借款信息")
+@Slf4j
 @RestController
-@RequestMapping("/borrowInfo")
+@RequestMapping("/api/core/borrowInfo")
 public class BorrowInfoController {
 
-}
+    @Resource
+    private BorrowInfoService borrowInfoService;
 
+    @ApiOperation("获取借款额度")
+    @GetMapping("/auth/getBorrowAmount")
+    public R getBorrowAmount(HttpServletRequest request) {
+        String token = request.getHeader("token");
+        Long userId = JwtUtils.getUserId(token);
+        BigDecimal borrowAmount = borrowInfoService.getBorrowAmount(userId);
+        return R.ok().data("borrowAmount", borrowAmount);
+    }
+
+    @ApiOperation("提交借款申请")
+    @PostMapping("/auth/save")
+    public R save(@RequestBody BorrowInfo borrowInfo, HttpServletRequest request) {
+        String token = request.getHeader("token");
+        Long userId = JwtUtils.getUserId(token);
+        borrowInfoService.saveBorrowInfo(borrowInfo, userId);
+        return R.ok().message("提交成功");
+    }
+
+    @ApiOperation("获取借款申请审批状态")
+    @GetMapping("/auth/getBorrowInfoStatus")
+    public R getBorrowerStatus(HttpServletRequest request) {
+        String token = request.getHeader("token");
+        Long userId = JwtUtils.getUserId(token);
+        Integer status = borrowInfoService.getStatusByUserId(userId);
+        return R.ok().data("borrowInfoStatus", status);
+    }
+}
--- service-core/src/main/java/com/atguigu/srb/core/service/BorrowInfoService.java
+++ service-core/src/main/java/com/atguigu/srb/core/service/BorrowInfoService.java
@@ -3,6 +3,8 @@ package com.atguigu.srb.core.service;
 import com.atguigu.srb.core.pojo.entity.BorrowInfo;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.math.BigDecimal;
+
 /**
  * <p>
  * 借款信息表 服务类
@@ -13,4 +15,27 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface BorrowInfoService extends IService<BorrowInfo> {
 
+    /**
+     * 获取借款额度
+     *
+     * @param userId /
+     * @return /
+     */
+    BigDecimal getBorrowAmount(Long userId);
+
+    /**
+     * 提交借款申请
+     *
+     * @param borrowInfo /
+     * @param userId     /
+     */
+    void saveBorrowInfo(BorrowInfo borrowInfo, Long userId);
+
+    /**
+     * 提交借款申请
+     *
+     * @param userId /
+     * @return /
+     */
+    Integer getStatusByUserId(Long userId);
 }
