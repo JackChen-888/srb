9ff6b69d66481c829770a952f6284928568b7c88
liuchen <liuchen525@126.com>
2021-07-09 16:36:24 +0800

    我要投资

--- service-core/src/main/java/com/atguigu/srb/core/controller/LendController.java
+++ service-core/src/main/java/com/atguigu/srb/core/controller/LendController.java
@@ -1,9 +1,17 @@
 package com.atguigu.srb.core.controller;
 
-
+import com.atguigu.common.result.R;
+import com.atguigu.srb.core.pojo.entity.Lend;
+import com.atguigu.srb.core.service.LendService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * <p>
@@ -13,9 +21,20 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Chen
  * @since 2021-05-21
  */
+@Api(tags = "标的")
 @RestController
-@RequestMapping("/lend")
+@RequestMapping("/api/core/lend")
+@Slf4j
 public class LendController {
 
+    @Resource
+    private LendService lendService;
+
+    @ApiOperation("获取标的列表")
+    @GetMapping("/list")
+    public R list() {
+        List<Lend> lendList = lendService.selectList();
+        return R.ok().data("lendList", lendList);
+    }
 }
 
